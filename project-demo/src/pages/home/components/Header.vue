67675a92d531e50509cc381892557f65b550cd70
hanweimin <hanweimin@15w.com>
2018-06-11 19:07:41 +0800

    project begin

---
+++ project-demo/src/pages/home/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header">
+    <div class="header-left">left</div>
+    <div class="header-input">input</div>
+    <div class="header-right">right</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  height: .86rem;
+ .header-left {
+   width: .64rem;
+   float: left;
+ }
+ .header-right {
+   width: 1.24rem;
+   float: right;
+ }
+}
+</style>

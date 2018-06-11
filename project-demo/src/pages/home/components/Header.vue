1fc99240ad3395ef426d1165e9c74f21c9d76c8f
Han Weimin <hanweimin1991@gmail.com>
2018-06-12 00:39:38 +0800

    homeHeader

--- project-demo/src/pages/home/components/Header.vue
+++ project-demo/src/pages/home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header">
-    <div class="header-left">left</div>
-    <div class="header-input">input</div>
-    <div class="header-right">right</div>
+    <div class="header-left">
+      <div class="iconfont icon-return">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont icon-arrow">&#xe6aa;</span>
+    </div>
   </div>
 </template>
 <script>
@@ -12,15 +20,39 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '~styles/varibles.scss';
 .header {
-  height: .86rem;
- .header-left {
-   width: .64rem;
-   float: left;
- }
- .header-right {
-   width: 1.24rem;
-   float: right;
- }
+  display: flex;
+  height: 0.86rem;
+  background: $bgcolor;
+  color: #fff;
+  line-height: 0.86rem;
+  .header-left {
+    width: 0.64rem;
+    float: left;
+    .icon-return {
+      font-size: 0.4rem;
+      text-align: center;
+    }
+  }
+  .header-right {
+    width: 1.24rem;
+    float: right;
+    text-align: center;
+    .icon-arrow {
+      font-size: 0.24rem;
+    }
+  }
+  .header-input {
+    flex: 1;
+    background: #fff;
+    border-radius: 0.1rem;
+    margin-top: 0.12rem;
+    height: 0.64rem;
+    line-height: 0.64rem;
+    margin-right: 0.2rem;
+    color: #ccc;
+    padding-left: 0.2rem;
+  }
 }
 </style>

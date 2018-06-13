15493aebbea47be493dcb801b97fc090f99f73c4
Han Weimin <hanweimin1991@gmail.com>
2018-06-14 03:24:05 +0800

    cityList

--- project-demo/src/pages/city/components/Header.vue
+++ project-demo/src/pages/city/components/Header.vue
@@ -1,5 +1,10 @@
 <template>
-    <div>this is city</div>
+  <div class="header">
+    城市选择
+    <router-link to="/">
+      <div class="iconfont header-back">&#xe624;</div>
+    </router-link>
+  </div>
 </template>
 
 <script>
@@ -9,5 +14,23 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+@import '~styles/varibles.scss';
+.header {
+  overflow: hidden;
+  height: .86rem;
+  line-height: .86rem;
+  text-align: center;
+  color: #fff;
+  background: $bgcolor;
+  font-size: .32rem;
+  .header-back{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: .64rem;
+    text-align: center;
+    font-size: .4rem;
+    color: #fff;
+  }
+}
 </style>
---
+++ project-demo/src/pages/city/components/Search.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="请输入城市名称或拼音">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~styles/varibles.scss';
+.search {
+  height: 0.72rem;
+  padding: .1rem;
+  background-color: $bgcolor;
+  .search-input {
+    box-sizing: border-box;
+    height: 0.62rem;
+    line-height: .62rem;
+    padding: 0 .1rem;
+    width: 100%;
+    text-align: center;
+    border-radius: .06rem;
+    color: #666;
+  }
+}
+</style>

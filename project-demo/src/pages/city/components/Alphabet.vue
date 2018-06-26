f5296c256fc32ba43a33c546a734a7451a090485
Han Weimin <hanweimin1991@gmail.com>
2018-06-27 03:27:43 +0800

    update

---
+++ project-demo/src/pages/city/components/Alphabet.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="alphabet">
+    <ul class="list">
+      <li class="item" v-for="(item, key) in cities" :key="key">{{key}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet',
+  props: {
+    cities: Object
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~styles/varibles.scss';
+.list {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  position: absolute;
+  top: 1.58rem;
+  right: 0;
+  bottom: 0;
+  width: .4rem;
+  .item {
+    text-align: center;
+    line-height: .44rem;
+    color: $bgcolor;
+  }
+}
+</style>
